259794d9d3d44451754cc7adeee05f6ff9e0ec6f
Angelo Rafael <angelorpt@gmail.com>
2018-12-08 11:06:27 -0400

    Usando a função Print() do Go

---
+++ Go Lang/02 - Print.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// Print sem quebra de linha
+	fmt.Print("Estamos usando uma função Print() em uma string.")
+
+	// quebra de linha e caracter de escape
+	fmt.Println("Cadeia de caracteres que possui \"quebra\" de linha") 
+
+	 // concatenação
+	fmt.Println("Angelo", "Rafael", "Pinto")
+}
