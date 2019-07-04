a7668213b2c7e82d961a5a3c5ec1303a1506d60f
Angelo Rafael <angelorpt@gmail.com>
2019-07-04 16:01:41 -0400

    - Criação de um Model (Contato)

--- Laravel/app/Http/Controllers/ContatoController.php
+++ Laravel/app/Http/Controllers/ContatoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Contato;
 
 class ContatoController extends Controller
 {
@@ -16,7 +17,13 @@ class ContatoController extends Controller
         $contatos2 = [
             (object)["nome"=>"Maria", "telefone"=>"1234564"],
             (object)["nome"=>"Pedro", "telefone"=>"6547894"]
-        ];        
+        ];   
+        
+        $objContato   = new Contato();
+        $listaContato = $objContato->lista();
+
+        dd($listaContato->nome);
+        
         return view('contato.index', compact('contatos','contatos2'));
     }
 
